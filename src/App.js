d7d80950305eb2440780cdf5be922426d12fbd25
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-10-07 17:51:02 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { Face } from "./Face"
+import styled from "styled-components"
+import { Shake } from "reshake"
 
+const Container = styled.div`
+  padding: 2em;
+  text-align: center;
+`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Container>
+        マウスを置いてね
+        <Shake h={80} v={60} r={50} dur={300} int={6} max={100}>
+          <Face />
+        </Shake>
+      </Container>
+    )
   }
 }
 
-export default App;
+export default App
